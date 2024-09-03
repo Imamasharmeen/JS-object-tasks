1c7cb812a9d7a9e5ccc2d5dbcc1cce223aeba783
Imamasharmeen <imamasharmeen@gmail.com>
2024-09-03 12:53:32 +0300

    Task4 done.

---
+++ task4.js
@@ -0,0 +1,13 @@
+/*Count the number of properties.*/
+
+
+
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+const numberOfProperties = Object.keys(student).length;
+
+console.log(numberOfProperties); 
